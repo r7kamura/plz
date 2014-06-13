62ca33e83211cf26261acf3d055d1855ee1c7deb
Ryo Nakamura <ryo-nakamura@cookpad.com>
2014-06-13 11:23:50 +0900

    Move all error classes into the error.rb

--- lib/plz/error.rb
+++ lib/plz/error.rb
@@ -1,4 +1,59 @@
 module Plz
   class Error < StandardError
+    USAGE = "Usage: plz <action> <target> [headers|params]"
+  end
+
+  class NoActionName < Error
+    def to_s
+      "You didn't pass action name\n#{USAGE}"
+    end
+  end
+
+  class NoTargetName < Error
+    def to_s
+      "You didn't pass target name\n#{USAGE}"
+    end
+  end
+
+  class SchemaFileNotFound < Error
+    def to_s
+      "Schema file was not found in #{SCHEMA_FILE_PATH_PATTERN}"
+    end
+  end
+
+  class InvalidSchema < Error
+    def initialize(pathname: pathname)
+      @pathname = pathname
+    end
+  end
+
+  class UndecodableSchemaFile < InvalidSchema
+    def to_s
+      "Failed to decode #{@pathname}"
+    end
+  end
+
+  class InvalidSchemaFile < InvalidSchema
+    def to_s
+      "#{@pathname} was invalid JSON Schema"
+    end
+  end
+
+  class BaseUrlNotFound < InvalidSchema
+    def to_s
+      "#{@pathname} has no base URL at top-level links property"
+    end
+  end
+
+  class LinkNotFound < InvalidSchema
+    def initialize(action_name: action_name, target_name: target_name, **args)
+      super(**args)
+      @action_name = action_name
+      @target_name = target_name
+    end
+
+    def to_s
+      "#{@pathname} has no definition for `#{@action_name} #{@target_name}`"
+    end
   end
 end
